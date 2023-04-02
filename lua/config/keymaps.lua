af461d8b252c21698ce5faa3149511cdb3f07eca
Grant Lanham <contact@grantlanham.com>
2023-04-01 22:10:17 -0400

    Add Iron REPL

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("v", "<leader>pmc", function()
+  require("iron.core").mark_visual()
+end, { noremap = true, silent = true, desc = "Iron mark_visual" })
+
+vim.keymap.set("v", "<leader>psc", function()
+  require("iron.core").visual_send()
+end, { noremap = true, silent = true, desc = "Iron visual_send" })
