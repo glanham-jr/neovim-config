2891a80317d65532c8bc21348bc98bc6551b1d46
Grant Lanham <contact@grantlanham.com>
2023-04-04 20:26:21 -0400

    Fix windows if statement

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-if vim.fn.has("win32") then
+
+if vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
   vim.opt.shell = "pwsh.exe"
 end
