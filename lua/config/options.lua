a66557879a1517db54b2870617a1543614d15d45
Grant Lanham <contact@grantlanham.com>
2024-01-08 09:45:24 -0500

    Fix .venv for Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,18 +2,24 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-if vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
+local is_windows = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1
+if is_windows then
   vim.opt.shell = "pwsh.exe"
 end
 
 local function file_exists(name)
   -- Must use full path
-  print(name)
+  -- print(name)
   local f = io.open(name, "r")
   return f ~= nil and io.close(f)
 end
 
-local python_prog_path = os.getenv("HOME") .. "/.venvs/.nvim-venv/bin/python"
+local python_prog_path = os.getenv("HOME")
+  .. "/.venvs/.nvim-venv/"
+  .. (is_windows and "Scripts" or "bin")
+  .. "/python"
+  .. (is_windows and ".exe" or "")
+
 if file_exists(python_prog_path) then
   vim.g.python_host_prog = python_prog_path
   vim.g.python3_host_prog = python_prog_path
