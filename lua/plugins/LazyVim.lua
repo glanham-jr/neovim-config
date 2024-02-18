31c7f697afa094df452c07a8ab150b7f9d35d68d
Grant Lanham <contact@grantlanham.com>
2024-02-18 17:13:45 -0500

    add additional lazy extras

--- lua/plugins/LazyVim.lua
+++ lua/plugins/LazyVim.lua
@@ -16,11 +16,4 @@ return {
       colorscheme = "gruvbox",
     },
   },
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
-  -- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
-
 }
