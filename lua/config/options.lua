27965e6b854c3ff371ce7d720e43436e9ca1b630
Grant Lanham Jr <contact@grantlanham.com>
2024-03-06 14:43:21 -0500

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.autoformat = false
+
 local is_windows = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1
 vim.g.is_windows = is_windows
 if is_windows then
