5ad360eb3ff0415fce9b2334c976e435c0fb7576
Grant Lanham <contact@grantlanham.com>
2025-01-05 16:51:25 -0500

    Add 4 spaces for tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,12 @@
 
 vim.g.autoformat = false
 
+-- By default, 4 spaces per tab is should be the default
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 local is_windows = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1
 vim.g.is_windows = is_windows
 if is_windows then
