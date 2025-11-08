cb22073afcccbdff8eef3cfbbe9998bce1ba05cf
Grant Lanham <contact@grantlanham.com>
2025-11-07 22:05:29 -0500

    fix: rename omnisharp to dotnet

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.dotnet" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
